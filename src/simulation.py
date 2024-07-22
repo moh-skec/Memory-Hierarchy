02bca6dfdb02d32889302afe1a1923cec23ebe0a
Mohammad Hossein Shahpoory <153901105+moh-skec@users.noreply.github.com>
2024-07-22 08:14:11 +0330

    The address accessor and cache contents getter methods were implemented.

--- src/simulation.py
+++ src/simulation.py
@@ -1,35 +1,31 @@
 # src/simulation.py
-import random
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
+from memory_hierarchy import CacheMemory
 
 
 class MemoryAccessSimulation:
     def __init__(self, memory_hierarchy):
         self.memory_hierarchy = memory_hierarchy
+        self.first_cache = memory_hierarchy[0]
+        self.hits = 0
+        self.misses = 0
+        self.access_time = 0
+        self.total_access_time = 0
+        self.accesses = 0
 
-    def generate_accesses(self, pattern, count):
-        addresses = []
-        if pattern == "sequential":
-            addresses = list(range(count))
-        elif pattern == "random":
-            addresses = [random.randint(
-                0, self.memory_hierarchy[-1].size) for _ in range(count)]
-        logging.debug(f"Generated Accesses: {addresses}")
-        return addresses
+    def access_address(self, address):
+        self.accesses += 1
+        _, _, access_time, name = self.first_cache.access(address)
+        self.access_time = access_time
+        self.total_access_time += access_time
+        if name in ["L1 Cache", "L2 Cache", "L3 Cache"]:
+            self.hits += 1
+            return True, _, name
+        self.misses += 1
+        return False, -1, "all cache levels"
 
-    def run_simulation(self, addresses):
-        results = {"hits": 0, "misses": 0, "access_times": []}
-        for address in addresses:
-            for level in self.memory_hierarchy:
-                _, hit, access_time = level.access(address)
-                results["access_times"].append(access_time)
-                if hit:
-                    results["hits"] += 1
-                    logging.debug(f"Address {address} Hit in {level.name}")
-                    break
-            else:
-                results["misses"] += 1
-                logging.debug(f"Address {address} Missed in All Levels")
-        return results
+    def get_cache_contents(self):
+        cache_contents = {}
+        for memory in self.memory_hierarchy:
+            if isinstance(memory, CacheMemory):
+                cache_contents[memory.name] = list(memory.cache.keys())
+        return cache_contents
