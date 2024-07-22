8ab515de0de53b65f667d5f37eea1791608c4705
Mohammad Hossein Shahpoory <153901105+moh-skec@users.noreply.github.com>
2024-07-22 08:07:41 +0330

    The access method was improved.

--- src/memory_hierarchy.py
+++ src/memory_hierarchy.py
@@ -1,14 +1,12 @@
 # src/memory_hierarchy.py
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
+from collections import OrderedDict
 
 
 class MemoryLevel:
-    def __init__(self, name, size, access_speed):
+    def __init__(self, name, size, access_time):
         self.name = name
         self.size = size
-        self.access_speed = access_speed
+        self.access_time = access_time
         self.data = {}
         self.access_count = 0
 
@@ -18,55 +16,58 @@ class MemoryLevel:
 
 
 class CacheMemory(MemoryLevel):
-    def __init__(self, name, size, access_speed, replacement_policy, lower_level=None):
-        super().__init__(name, size, access_speed)
+    def __init__(self, name, size, access_time, block_size, replacement_policy, lower_level=None):
+        super().__init__(name, size, access_time)
+        self.block_size = block_size
         self.replacement_policy = replacement_policy(self)
-        self.cache = {}
+        self.cache = OrderedDict()
         self.lower_level = lower_level
 
     def access(self, address):
         self.access_count += 1
-        if address in self.cache:
-            self.replacement_policy.hit(address)
-            logging.debug(f"Cache Hit: {address} in {self.name}")
-            return self.cache[address], True, self.access_speed
+        block_address = address // self.block_size
+        if block_address in self.cache:
+            self.replacement_policy.hit(block_address)
+            return self.cache[block_address], True, self.access_time, self.name
         else:
-            if len(self.cache) >= self.size:
-                self.replacement_policy.evict()
-            _, _, lower_access_speed = self.lower_level.access(address)
-            self.cache[address] = f"Data at {address}"
-            self.replacement_policy.miss(address)
-            logging.debug(f"Cache Miss: {address} in {self.name}")
-            return self.cache[address], False, self.access_speed + lower_access_speed
+            evicted_block = block_address
+            if len(self.cache) >= self.size // self.block_size:
+                evicted_block = self.replacement_policy.evict()
+            data, _, lower_access_time, name = self.lower_level.access(address)
+            self.replacement_policy.order = OrderedDict([(block_address, data) if k == evicted_block else (
+                k, v) for k, v in self.replacement_policy.order.items()])
+            self.replacement_policy.miss(block_address, data)
+            return data, False, self.access_time + lower_access_time, name
 
 
 class MainMemory(MemoryLevel):
-    def __init__(self, name, size, access_speed, lower_level=None):
-        super().__init__(name, size, access_speed)
+    def __init__(self, name, size, access_time, lower_level=None):
+        super().__init__(name, size, access_time)
         self.lower_level = lower_level
 
     def access(self, address):
         self.access_count += 1
         if address in self.data:
-            logging.debug(f"MainMemory Hit: {address}")
-            return self.data[address], True, self.access_speed
+            return self.data[address], True, self.access_time, self.name
         else:
             if self.lower_level:
-                _, _, lower_access_speed = self.lower_level.access(address)
+                data, _, lower_access_time, name = self.lower_level.access(
+                    address)
             else:
-                lower_access_speed = 0
-            self.data[address] = f"Data at {address}"
-            logging.debug(f"MainMemory Miss: {address}")
-            return self.data[address], False, self.access_speed + lower_access_speed
+                data, lower_access_time = f"Data at {address}", 0
+            self.data[address] = data
+            return data, False, self.access_time + lower_access_time, name
 
 
 class ExternalMemory(MemoryLevel):
+    def __init__(self, name, size, access_time):
+        super().__init__(name, size, access_time)
+
     def access(self, address):
         self.access_count += 1
         if address in self.data:
-            logging.debug(f"ExternalMemory Hit: {address}")
-            return self.data[address], True, self.access_speed
+            return self.data[address], True, self.access_time, self.name
         else:
-            self.data[address] = f"Data at {address}"
-            logging.debug(f"ExternalMemory Miss: {address}")
-            return self.data[address], False, self.access_speed
+            data = f"Data at {address}"
+            self.data[address] = data
+            return data, False, self.access_time, "None"
